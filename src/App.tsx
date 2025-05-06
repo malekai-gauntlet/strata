079e18b3f72d0109420bf53c0d33ea311b0dd105
Malekai Mischke <malekai.mischke@gauntletai.com>
2025-05-05 22:40:21 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ import Index from "./pages/Index";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Flyer from "./pages/Flyer";
-import Landing from "./pages/Landing";
-import Coaches from "./pages/Coaches";
-import Coach from "./pages/coach";
 import Test from "./pages/Test";
 
 const queryClient = new QueryClient();
@@ -24,9 +21,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/flyer" element={<Flyer />} />
-          <Route path="/landing" element={<Landing />} />
-          <Route path="/coaches" element={<Coaches />} />
-          <Route path="/coach" element={<Coach />} />
           <Route path="/test" element={<Test />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
