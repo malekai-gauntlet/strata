42b552db79a0479944ec961dd8f425b09315df6e
dannygauntletai <danny.mota@gauntletai.com>
2025-05-16 12:41:11 -0500

    add hotjar and mouseflow

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,78 @@ import Parents from "./pages/Parents";
 import Academics from "./pages/Academics";
 import ESAs from "./pages/ESAs";
 import IMGPage from "./pages/IMGPage";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
+
+// Hotjar tracking component
+const HotjarTracking = () => {
+  useEffect(() => {
+    // Hotjar Tracking Code
+    const hotjarScript = document.createElement('script');
+    hotjarScript.innerHTML = `
+      (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:6406153,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    `;
+    document.head.appendChild(hotjarScript);
+
+    return () => {
+      try {
+        if (hotjarScript.parentNode) {
+          hotjarScript.parentNode.removeChild(hotjarScript);
+        }
+      } catch (e) {
+        console.error('Error removing Hotjar script:', e);
+      }
+    };
+  }, []);
+
+  return null;
+};
+
+// Mouseflow tracking component
+const MouseflowTracking = () => {
+  useEffect(() => {
+    // Mouseflow Tracking Code
+    const mfScript = document.createElement('script');
+    mfScript.type = "text/javascript";
+    mfScript.defer = true;
+    mfScript.innerHTML = `
+      window._mfq = window._mfq || [];
+      (function() {
+        var mf = document.createElement("script");
+        mf.type = "text/javascript"; mf.defer = true;
+        mf.src = "//cdn.mouseflow.com/projects/093468ee-8454-4313-9398-5880a29cef27.js";
+        document.getElementsByTagName("head")[0].appendChild(mf);
+      })();
+    `;
+    document.head.appendChild(mfScript);
+
+    return () => {
+      try {
+        if (mfScript.parentNode) {
+          mfScript.parentNode.removeChild(mfScript);
+        }
+      } catch (e) {
+        console.error('Error removing Mouseflow script:', e);
+      }
+    };
+  }, []);
+
+  return null;
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <HotjarTracking />
+      <MouseflowTracking />
       <Toaster />
       <Sonner />
       <BrowserRouter>
